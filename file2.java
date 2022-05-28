3ca8c9dc5686863222d80d58d21f53e9015f4974
ianjan10 <106170477+ianjan10@users.noreply.github.com>
2022-05-28 21:51:04 +0530

    Create file2.java

---
+++ file2.java
@@ -0,0 +1,9 @@
+class file2
+{
+    public static void main(String args[])
+    {
+        file1 f = new file1();
+        String str2 = f.input();
+        System.out.println("the string is" + str2);
+    }
+} 
